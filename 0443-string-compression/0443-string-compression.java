bb512166f3e60fc65a0f3666a959f2b7728cb0cd
Palak Garg <75618338+palakkk27@users.noreply.github.com>
2023-03-12 09:38:59 +0530

    Time: 1 ms (95.98%), Space: 42.3 MB (44.34%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,22 +1,22 @@
 class Solution {
     public int compress(char[] chars) {
-        int newindex=0; 
-        for(int i=0; i<chars.length; i++){
-            Integer count=1; 
-            while( i<chars.length-1 && chars[i]==chars[i+1]){  //while same characters are                                                                  found 
-                count++;
-                i++;
+        int index=0;
+        int i=0;
+        while(i<chars.length){
+            int j=i;
+            while(j<chars.length && chars[j] == chars[i]){
+                j++;
             }
-            chars[newindex] = chars[i];
-            newindex++;
-            if(count>1){
-                for(char ch: Integer.toString(count).toCharArray()){ 
-                    chars[newindex] = ch;
-                    newindex++;
+            chars[index++] = chars[i];
+            if(j-i>1){
+                String count = j-i+"";
+                for(char c: count.toCharArray()){
+                    chars[index++]=c;
                 }
-
             }
+            i=j;
         }
-        return newindex;    
-    }    
+        return index;
+        
+    }
 }
