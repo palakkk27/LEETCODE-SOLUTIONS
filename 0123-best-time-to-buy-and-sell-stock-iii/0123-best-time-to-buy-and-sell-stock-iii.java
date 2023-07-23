85df737e9d4bb12eb1b2728f3696201809026775
Palak Garg <75618338+palakkk27@users.noreply.github.com>
2023-07-23 16:09:35 +0530

    Time: 86 ms (32.48%), Space: 62.2 MB (40.04%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,26 +1,30 @@
 class Solution {
+    //Tablulation form
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int dp[][][] = new int[n+1][2][3];
-        for(int[][] row : dp) for(int[] col : row) Arrays.fill(col, -1);
-        return helper(prices,0,1,2,dp);
+        
+        return helper(prices);
     }
-    public int helper(int[] prices, int index, int buy, int limit,int[][][] dp){
-        // Base Case
-        if(index == prices.length) return 0;
-        if(limit == 0) return 0;
-        if(dp[index][buy][limit]!=-1) return dp[index][buy][limit];
-
+    public int helper(int[] prices){
+        int[][][] dp = new int[prices.length+1][2][3];
+        for(int[][] row : dp) for(int[] col : row) Arrays.fill(col, 0);
+        
+         for(int index = prices.length - 1; index >= 0; index--){
+            for(int buy = 0; buy < 2; buy++){
+                for(int limit = 1; limit < 3; limit++){
         int totalProfit = 0;
         if(buy == 1){
-            int buyKaro = -prices[index] + helper(prices, index + 1, 0, limit,dp);
-            int skipKaro = helper(prices, index + 1, 1, limit,dp);
-            totalProfit = Math.max(buyKaro, skipKaro);
-        }else{
-            int sellKaro = prices[index] + helper(prices, index + 1, 1, limit - 1,dp);
-            int skipKaro = helper(prices, index + 1, 0, limit,dp);
-            totalProfit = Math.max(sellKaro, skipKaro);
-        }
-        return dp[index][buy][limit] = totalProfit;
+                        int buyKaro = -prices[index] + dp[index + 1][0][limit];
+                        int skipKaro = dp[index + 1][1][limit];
+                        totalProfit = Math.max(buyKaro, skipKaro);
+                    }else{
+                        int sellKaro = prices[index] + dp[index + 1][1][limit - 1];
+                        int skipKaro = dp[index + 1][0][limit];
+                        totalProfit = Math.max(sellKaro, skipKaro);
+                    }
+                    dp[index][buy][limit] = totalProfit;
+                }
+            }
+    }
+        return dp[0][1][2];
     }
 }
