ed086a4d2babc95bd941247808db288a48845b94
Palak Garg <75618338+palakkk27@users.noreply.github.com>
2023-08-30 17:00:24 +0530

    Time: 4 ms (79.74%), Space: 41.9 MB (90.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,32 +1,46 @@
 import java.util.*;
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
+        if(s.length()!=t.length())
             return false;
-        }
         
-        HashMap<Character,Integer> hm = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch=s.charAt(i);
-            if(hm.containsKey(s.charAt(i))){
-                hm.put(ch,hm.get(ch)+1);
-            }else{
-                hm.put(ch,1);
-            }
+        
+        int[] freq = new int[26];
+        for(int i = 0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
         }
-         for (int i = 0; i < t.length(); i++) {
-            char ch=t.charAt(i);
-            if(hm.get(ch)!=null){
-                if(hm.get(ch)==1){
-                    hm.remove(ch);
-                }else{
-                    hm.put(ch,hm.get(ch)-1);
-                }
-            }else{
-                return false;
-            }
+        for(int i = 0;i<t.length();i++){
+            freq[t.charAt(i)-'a']--;
         }
-        return hm.isEmpty();
+        for(int i = 0;i<26;i++){
+            if(freq[i]!=0) return false;
+        }
+        return true;
     }
 }
+        
+//         HashMap<Character,Integer> hm = new HashMap<>();
+
+//         for (int i = 0; i < s.length(); i++) {
+//             char ch=s.charAt(i);
+//             if(hm.containsKey(s.charAt(i))){
+//                 hm.put(ch,hm.get(ch)+1);
+//             }else{
+//                 hm.put(ch,1);
+//             }
+//         }
+//          for (int i = 0; i < t.length(); i++) {
+//             char ch=t.charAt(i);
+//             if(hm.get(ch)!=null){
+//                 if(hm.get(ch)==1){
+//                     hm.remove(ch);
+//                 }else{
+//                     hm.put(ch,hm.get(ch)-1);
+//                 }
+//             }else{
+//                 return false;
+//             }
+//         }
+//         return hm.isEmpty();
+//     }
+// }
